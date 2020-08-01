2ee1d49b84f6f8f82e0328319b371f53cbfe2829
Deepak Singh Maurya <mauryagdeepak@gmail.com>
2020-08-01 16:27:22 +0530

    Updates

---
+++ precache-manifest.7bc5b04b990320820c84a552f6c21155.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b5114ddf71ccac0922b67593b84ab54",
+    "url": "/React-ToDoList-live-Demo/index.html"
+  },
+  {
+    "revision": "0f0da23636f7dbd354af",
+    "url": "/React-ToDoList-live-Demo/static/css/main.4f8628ef.chunk.css"
+  },
+  {
+    "revision": "f0fadf413bc81a054e2a",
+    "url": "/React-ToDoList-live-Demo/static/js/2.6c58c6de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-ToDoList-live-Demo/static/js/2.6c58c6de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f0da23636f7dbd354af",
+    "url": "/React-ToDoList-live-Demo/static/js/main.f8f73485.chunk.js"
+  },
+  {
+    "revision": "8404b077ae21af58b2cf",
+    "url": "/React-ToDoList-live-Demo/static/js/runtime-main.33be8f26.js"
+  }
+]);
